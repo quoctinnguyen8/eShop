1e722c70dd892a6438baf6d4c2cda42a087f1f21
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2022-08-28 20:56:46 +0700

    tạo viewmodel

--- eShop/eShop/Program.cs
+++ eShop/eShop/Program.cs
@@ -29,6 +29,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+	name: "AdminArea",
+	areaName: "Admin",
+	pattern: "/Admin/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
